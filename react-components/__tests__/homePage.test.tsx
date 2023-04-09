0665e3d403974716321f215291bd0ba9d5d4aa86
Lex <pineapplepear55@gmail.com>
2023-04-09 20:49:04 +0300

    feat: remove comments

--- react-components/__tests__/homePage.test.tsx
+++ react-components/__tests__/homePage.test.tsx
@@ -4,18 +4,6 @@ import '@testing-library/jest-dom/extend-expect';
 import HomePage from '../src/routes/homePage';
 import 'jest-localstorage-mock';
 
-import 'whatwg-fetch';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { MockSearchResponse } from './__mocks__/mockSearchResponse';
-import { MockCardResponse } from './__mocks__/mockCardResponse';
-
-const server = setupServer(
-  rest.get('https://api.flickr.com/services/rest/', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(MockSearchResponse));
-  })
-);
-
 describe('HomePage bar rendering', () => {
   test('renders the home page', () => {
     render(<HomePage />);
@@ -61,26 +49,5 @@ describe('Card data API', () => {
       const modal = screen.getByTestId('card-modal-title');
       expect(modal).toBeInTheDocument();
     });
-    // userEvent.keyboard('[Enter]');
   });
-
-  // test('handles failure', async () => {
-  //   server.use(
-  //     rest.get('https://api.flickr.com/services/rest/', (req, res, ctx) => {
-  //       return res(ctx.status(404), ctx.json({ error: 'error' }));
-  //     })
-  //   );
-  //   await waitFor(() => {
-  //     const errorContainer = screen.getByTestId('error-container');
-  //     expect(errorContainer).toBeInTheDocument();
-  //   });
-  // });
-
-  // test('should fetch', async () => {
-  //   const result = await fetch(
-  //     `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b158ecdf6b84a3ae427372f61ddab5b7&text=kitten&per_page=10&format=json&nojsoncallback=1`
-  //   );
-  //   console.log(result);
-
-  // });
 });

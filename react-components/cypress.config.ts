74121a9adadeed1913e793af2b940a450fd213e6
Lex <pineapplepear55@gmail.com>
2023-05-02 23:43:54 +0300

    feat: change cypress timeout

--- react-components/cypress.config.ts
+++ react-components/cypress.config.ts
@@ -4,6 +4,7 @@ import codeCoverage from '@cypress/code-coverage/task';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       codeCoverage(on, config);
       return config;
